c34a07c814c92a9c3b5a4754db9de15d48f8969c
Apurv Shah <43690373+ApurvShah007@users.noreply.github.com>
2020-07-01 17:17:47 -0500

    Update rolling_stats.py
    Additional documentation for the functions and solves the optionally return the data frame problem.

--- rolling_stats.py
+++ rolling_stats.py
@@ -9,28 +9,58 @@ import os
 pd.options.display.float_format = '{:.3f}'.format
 
 
+#This file is to be used for further biuild upon the given functions. These functions are commonly used in many regular 
+#computations. These are some of the common functions
+
+
+#Assumes that the csv file is in the same folder as this file. 
 def symbol_to_path(symbol):
     return os.path.join("{}.csv".format(str(symbol)))
 
-def daily_return(df):
- 	df_return = df.copy()
- 	df_return = df_return.rename(columns = {"Adj Close" : "Daily Return"})
- 	df_return[1:] = (df[1:]/df[:-1].values)-1
- 	df_return.iloc[0] = 0
- 	df_return['Daily Return'] = df_return['Daily Return']*100
- 	ax = df_return.plot(title = "Daily Returns Plot")
- 	plt.show()
 
+#This function returns or plots the daily returns of a given stock for a given time period. 
+#This function accepts 3 parameters. df - the historic dataframe of the stock, date- the daterange for the plot, plot - a boolean for
+#whether the user wnats to see the plot or just wants the resulting dataframe.  
 
-def cumulative_return(df):
+def daily_return(df,dates, plot):
+	df_return = df.copy()
+	df_return = df_return.rename(columns = {"Adj Close" : "Daily Return"})
+	df_exp = pd.DataFrame(index = dates)
+	df_exp = df_exp.join(df_return)
+	df_exp.dropna(inplace = True)
+	df_return[1:] = (df_exp[1:]/df_exp[:-1].values)-1
+	df_return.iloc[0] = 0
+	if(plot==True):
+ 		df_return['Daily Return'] = df_return['Daily Return']*100
+ 		df_return['Daily Return'].plot(title = "Daily Returns")
+ 		plt.show()
+	if (plot==False): 
+ 		return df_return
+
+#This function returns or plots the cumilative returns of a given stock for a given time period. 
+#This function accepts 3 parameters. df - the historic dataframe of the stock, date- the daterange for the plot, plot - a boolean for whether the user 
+#wants to see the plot or just wants the resulting dataframe.  
+#This function treats the first reading of the diven daterange as 0 and calculates the returns with respect to that date. 
+
+def cumulative_return(df,date,plot):
  	df_cr = df.copy()
  	df_cr=df_cr.rename(columns = {"Adj Close" : "Cumilative Return"})
- 	df_cr[1:] = (df[1:]/df.iloc[0])-1
+ 	df_exp = pd.DataFrame(index = dates)
+ 	df_exp = df_exp.join(df_return)
+ 	df_cr[1:] = (df_exp[1:]/df_exp.iloc[0])-1
  	df_cr.iloc[0] = 0
- 	df_cr = df_cr['Cumilative Return']*100
- 	ax = df_cr.plot(title = "Cumm Return")
- 	plt.show()
+ 	if (plot==False):
+ 		return df_cr
+ 	else:
+ 	 	df_cr = df_cr['Cumilative Return']*100
+ 	 	ax = df_cr.plot(title = "Cumm Return")
+ 	 	plt.show()
 
+
+#This function calculates the bollinger bands for the given stock in the given time period. These bands are powerful indicators for 
+#making the decision for selling and buying. This should not be your only indicator but it certainly helps to visualize the data and trends.
+#The upper bound is mean+2*std and the lower bound is mean-2*std. These lines are plotted on the graph along with the prices. 
+#The rolling mean and rolling std is calculated for the past 20 days. 
 
 def bollinger_bands(df):
 	rm = df.rolling(20).mean()
@@ -47,14 +77,16 @@ def bollinger_bands(df):
 	plt.show()
 
 
-#Sample Run
-path = symbol_to_path('AZPN')
-df_AZPN = pd.read_csv(path, index_col="Date",parse_dates=True, usecols=["Date", "Adj Close"])
-df_AZPN = df_AZPN.iloc[::-1]
+#Sample Run with a random stock
 
-dates_2017 = pd.date_range('2017-01-01','2017-12-31')
-df_2017 = pd.DataFrame(index = dates_2017)
-df_2017 = df_2017.join(df_AZPN)
-df_2017 = df_2017.dropna()
-cumulative_return(df_2017)
+# path = symbol_to_path('AZPN')
+# df_AZPN = pd.read_csv(path, index_col="Date",parse_dates=True, usecols=["Date", "Adj Close"])
+# df_AZPN = df_AZPN.iloc[::-1]
+
+# dates_2017 = pd.date_range('2017-01-01','2017-12-31')
+# df_2017 = pd.DataFrame(index = dates_2017)
+# df_2017 = df_2017.join(df_AZPN)
+# df_2017 = df_2017.dropna()
+# cumulative_return(df_2017)
+
 
