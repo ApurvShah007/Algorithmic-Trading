7e32226aa1b1c7ea6f57457ab0ce5a3cffa0fb99
Apurv Shah <apurvshah@Apurvs-MBP.home>
2020-07-13 15:56:29 +0300

    Major changes in code presentation and inclusion of yfinance and corresponding changes in the code fixes #5 fixes #8

--- Code/norm_dist.py
+++ Code/norm_dist.py
@@ -5,43 +5,46 @@ import numpy as np
 import talib
 import statsmodels.api as sm 
 import time 
+import yfinance as yf
 import os
+
+
 pd.options.display.float_format = '{:.5f}'.format
+def norm_dist_yearwise(symbol, dates, plot=False):
+    '''
+    This function first slices out the daterange provided and then uses the first value at the 0th row of the new sliced dataframe as the normalizing value and then 
+    normalizes the entire ditribution based on that and then plots it. 
+    This function accepts the list of stocks and the date range of the required plot. 
+    '''
+    df = get_data_close(symbol)
+    df_nor = pd.DataFrame(index = dates)
+    df = df.rename(columns = {"Close" : symbol})
+    df_nor = df_nor.join(df)
+    df_nor = df_nor.dropna()
+    #Normalizing the Ditribution
+    df_nor= (df_nor)/df_nor.iloc[0]-1
+    if (plot==True):
+        df_nor.plot()
+        plt.show()
+    return df_nor
 
-#Assumes that the csv file is in the same folder as the 
-def symbol_to_path(symbol):
-    return os.path.join("/Users/apurvshah/Desktop/Algorithmic_trading/Data/{}.csv".format(str(symbol)))
 
-#This function treats the first ever recorded stock price to be the initial value and the rest of the ditribution is accordingly normalized. Then the selected date 
-#range is sliced out and the plot is shown. 
-#This function accepts the list of stocks and the date range of the required plot. 
 
-def norm_dist(stocks, symbol, dates, plot = False):
-	df_nor = pd.DataFrame(index = dates)
-	df = df.rename(columns = {"Adj Close" : s})
-	#Normalizing the Ditribution
-	df =(df)/df.iloc[0]	
-	df_nor = df_nor.join(df)
-	df_nor.dropna(inplace = True)
-	if plot ==True:
-		df_nor.plot()
-		plt.show()
-	return df_nor
-	
-#This function first slices out the daterange provided and then uses the first value at the 0th row of the new sliced dataframe as the normalizing value and then 
-#normalizes the entire ditribution based on that and then plots it. 
-#This function accepts the list of stocks and the date range of the required plot. 
-def norm_dist_yearwise(df, symbol, dates, plot=False):
-	df_nor = pd.DataFrame(index = dates)
-	df = df.rename(columns = {"Adj Close" : symbol})
-	df_nor = df_nor.join(df)
-	df_nor = df_nor.dropna()
-	#Normalizing the Ditribution
-	df_nor= (df_nor)/df_nor.iloc[0]
-	if (plot==True):
-		df_nor.plot()
-		plt.show()
-	return df_nor
-
-	
+def norm_dist(symbol, dates, plot = False):
+    '''    
+    This function treats the first ever recorded stock price to be the initial value and the rest of the ditribution is accordingly normalized. Then the selected date 
+    range is sliced out and the plot is shown. 
+    This function accepts the list of stocks and the date range of the required plot. 
+    '''
+    df = get_data_close(symbol)
+    df_nor = pd.DataFrame(index = dates)
+    df = df.rename(columns = {"Close" : symbol})
+    #Normalizing the Ditribution
+    df =(df)/df.iloc[0]-1
+    df_nor = df_nor.join(df)
+    df_nor.dropna(inplace = True)
+    if plot ==True:
+        df_nor.plot()
+        plt.show()
+    return df_nor
 
--- Code/rolling_stats.py
+++ Code/rolling_stats.py
@@ -4,85 +4,78 @@ import seaborn as sns
 import numpy as np
 import talib
 import statsmodels.api as sm 
+import time 
+import yfinance as yf
 import os
 
-pd.options.display.float_format = '{:.3f}'.format
+pd.options.display.float_format = '{:.5f}'.format
 
 
-#This file is to be used for further biuild upon the given functions. These functions are commonly used in many regular 
-#computations. These are some of the common functions
+def daily_return(symbol, dates, plot= False):
+    '''
+    This function returns or plots the daily returns of a given stock for a given time period. 
+    This function accepts 3 parameters. df - the historic dataframe of the stock, date- the daterange for the plot, plot - a boolean for
+    whether the user wnats to see the plot or just wants the resulting dataframe.  
+    '''
+    df_return = get_data_close(symbol)
+    df_exp = pd.DataFrame(index = dates)
+    df_exp = df_exp.join(df_return)
+    df_exp.dropna(inplace = True)
+    df_return = df_exp
+    df_return = df_return.rename(columns = {'Close': "Daily Return"})
+    df_return[1:] = (df_exp[1:]/df_exp[:-1].values)-1
+    df_return.iloc[0] = 0
+    if(plot==True):
+        df_return['Daily Return'] = df_return['Daily Return']
+        df_return['Daily Return'].plot(title = "Daily Returns")
+        plt.show()
+    df_return['Daily Return'] = df_return['Daily Return']
+    return df_return
+
+def cumulative_return(symbol,dates ,plot=False):
+    '''
+    This function calculates the bollinger bands for the given stock in the given time period. These bands are powerful indicators for 
+    making the decision for selling and buying. This should not be your only indicator but it certainly helps to visualize the data and trends.
+    The upper bound is mean+2*std and the lower bound is mean-2*std. These lines are plotted on the graph along with the prices. 
+    The rolling mean and rolling std is calculated for the past 20 days. 
+    '''
+    df = get_data_close(symbol)
+    df_exp = pd.DataFrame(index = dates)
+    df_exp = df_exp.join(df)
+    df_exp.dropna(inplace=True)
+    df_cr = df_exp
+    df_cr=df_cr.rename(columns = { 'Close': "Cumulative Return"})
+    df_cr[1:] = (df_exp[1:]/df_exp.iloc[0])-1
+    df_cr.iloc[0] = 0
+    if plot== True:
+        df_cr['Cumulative Return'] = df_cr['Cumulative Return']
+        df_cr.plot(title = "Cumulative Return")
+        plt.show()
+    return df_cr
 
 
-#Assumes that the csv file is in the same folder as the 
-def symbol_to_path(symbol):
-    return os.path.join("/Users/apurvshah/Desktop/Algorithmic_trading/Data/{}.csv".format(str(symbol)))
+def bollinger_bands(symbol, dates , rolling_range = 20):
+    ''' 
+    This function calculates the bollinger bands for the given stock in the given time period. These bands are powerful indicators for 
+    making the decision for selling and buying. This should not be your only indicator but it certainly helps to visualize the data and trends.
+    The upper bound is mean+2*std and the lower bound is mean-2*std. These lines are plotted on the graph along with the prices. 
+    The rolling mean and rolling std is calculated for the past 20 days by default but can be changed. 
+    '''
+    df_exp = pd.DataFrame(index = dates)
+    df = get_data_close(symbol)
+    df = df_exp.join(df)
+    df.dropna(inplace=True)
+    rm = df.rolling(rolling_range).mean()
+    rstd =df.rolling(rolling_range).std()
+    ub = rm+ 2*rstd
+    lb = rm- 2*rstd
+    ax = df.plot(title = "Bollinger Bounds")
+    rm=rm.rename(columns = {"Adj Close" : "Rolling Mean "})
+    rm.plot(label = "Rolling Mean", ax=ax)
+    ub=ub.rename(columns = {"Adj Close" : "Upper Bound"})
+    ub.plot(label = "Upper Bound", ax=ax)
+    lb=lb.rename(columns = {"Adj Close" : "Lower Bound"})
+    lb.plot(label = "Lower Bound", ax=ax)
+    plt.show()
 
-
-#This function returns or plots the daily returns of a given stock for a given time period. 
-#This function accepts 3 parameters. df - the historic dataframe of the stock, date- the daterange for the plot, plot - a boolean for
-#whether the user wnats to see the plot or just wants the resulting dataframe.  
-
-def daily_return(df,column, dates, plot= False):
-	df_return = df.copy()
-	df_exp = pd.DataFrame(index = dates)
-	df_exp = df_exp.join(df_return)
-	df_exp.dropna(inplace = True)
-	df_return = df_exp
-	df_return = df_return.rename(columns = {column: "Daily Return"})
-	df_return[1:] = (df_exp[1:]/df_exp[:-1].values)-1
-	df_return.iloc[0] = 0
-	if(plot==True):
-		df_return['Daily Return'] = df_return['Daily Return']*100
-		df_return['Daily Return'].plot(title = "Daily Returns")
-		plt.show()
-	df_return['Daily Return'] = df_return['Daily Return']*100
-	return df_return
-
-#This function returns or plots the cumilative returns of a given stock for a given time period. 
-#This function accepts 3 parameters. df - the historic dataframe of the stock, date- the daterange for the plot, plot - a boolean for
-#whether the user wnats to see the plot or just wants the resulting dataframe.  
-#This function treats the first reading of the diven daterange as 0 and calculates the returns with respect to that date. 
-def cumulative_return(df,column,dates ,plot=False):
-	df_exp = pd.DataFrame(index = dates)
-	df_exp = df_exp.join(df)
-	df_exp.dropna(inplace=True)
-	df_cr = df_exp
-	df_cr=df_cr.rename(columns = { column: "Cumilative Return"})
-	df_cr[1:] = (df_exp[1:]/df_exp.iloc[0])-1
-	df_cr.iloc[0] = 0
-	if plot== True:
- 	 	df_cr['Cumilative Return'] = df_cr['Cumilative Return']*100
- 	 	ax = df_cr.plot(title = "Cumm Return")
- 	 	plt.show()
-	return df_cr
-#This function calculates the bollinger bands for the given stock in the given time period. These bands are powerful indicators for 
-#making the decision for selling and buying. This should not be your only indicator but it certainly helps to visualize the data and trends.
-#The upper bound is mean+2*std and the lower bound is mean-2*std. These lines are plotted on the graph along with the prices. 
-#The rolling mean and rolling std is calculated for the past 20 days. 
-def bollinger_bands(df):
-	rm = df.rolling(20).mean()
-	rstd =df.rolling(20).std()
-	ub = rm+ 2*rstd
-	lb = rm- 2*rstd
-	ax = df.plot(title = "Bollinger Bounds")
-	rm=rm.rename(columns = {"Adj Close" : "Rolling Mean "})
-	rm.plot(label = "Rolling Mean", ax=ax)
-	ub=ub.rename(columns = {"Adj Close" : "Upper Bound"})
-	ub.plot(label = "Upper Bound", ax=ax)
-	lb=lb.rename(columns = {"Adj Close" : "Lower Bound"})
-	lb.plot(label = "Lower Bound", ax=ax)
-	plt.show()
-
-
-#Sample Run with a random stock
-
-# path = symbol_to_path('AZPN')
-# df_AZPN = pd.read_csv(path, index_col="Date",parse_dates=True, usecols=["Date", "Adj Close"])
-# df_AZPN = df_AZPN.iloc[::-1]
-
-# dates_2017 = pd.date_range('2017-01-01','2017-12-31')
-# df_2017 = pd.DataFrame(index = dates_2017)
-# df_2017 = df_2017.join(df_AZPN)
-# df_2017 = df_2017.dropna()
-# bollinger_bands(df_2017)
 
